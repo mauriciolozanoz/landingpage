50a548816e865ae7a85021a5dbfd6ed4791c0aaa
Mauricio Lozano Zarate <mauricio.lozano@crediclub.com>
2024-01-31 14:17:08 -0600

    Version 4 PYME

--- pyme/pyme.js
+++ pyme/pyme.js
@@ -68,3 +68,29 @@ const observeranimation = new IntersectionObserver((entries) => {
   // Agrega aquí el elemento o elementos a observar
   observeranimation.observe(document.getElementById('counter')); // Asegúrate de que este es el ID correcto
   observeranimation.observe(document.getElementById('counter5'));
+
+
+  const scrollers = document.querySelectorAll('.scroller');
+
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+
+    const scrollerInner =  scroller.querySelector('.scroller__inner');
+    const scrollerInnerContent = Array.from(scrollerInner.children);
+
+    scrollerInnerContent.forEach(item => {
+      const dupilcatedItem = item.cloneNode(true)
+      // console.log(dupilcatedItem);
+
+      dupilcatedItem.setAttribute('aria-hidden', true)
+
+      scrollerInner.appendChild(dupilcatedItem)
+    })
+  })
+}
+
+addAnimation()
